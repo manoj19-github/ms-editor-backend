e11c240adc6793d0a8fda197e9cd1ab97b817887
manoj-fleapo <106957653+manoj-fleapo@users.noreply.github.com>
2022-08-12 10:17:27 +0530

    intermediate push

--- socket.ts
+++ socket.ts
@@ -2,11 +2,16 @@ import http from "http";
 import {Server,Socket} from "socket.io";
 import { SOCKET_ACTIONS } from "./utils/socketAction";
 let IO:any;
+const userSocketMap = {}
+
 export default(server:http.Server)=>{
     const io = new Server(server,{
         pingTimeout:12000,
         cors:{origin:process.env.CLIENT_APP_URL}
     })
+    const allConnectedClients = (roomId:string)=>{
+        return Array.from(io.sockets.adapter.rooms.get(roomId) || []);
+    }
     IO = io.on("connection",(socket:Socket)=>{
         console.log(`connected to socket io on ms-editor`)
         socket.emit(SOCKET_ACTIONS.SERVER_UP,{message:`Server is up at port ${process.env.PORT} `})
@@ -14,8 +19,9 @@ export default(server:http.Server)=>{
             console.log(message)
         })
         socket.on(SOCKET_ACTIONS.JOIN,({roomId,userName}:{roomId:string;userName:string})=>{
-            console.log("roomId : ",roomId);
-            console.log("userName : ",userName);
+            socket.join(roomId);
+            userSocketMap[userName] = socket.id;
+            const clients = allConnectedClients(roomId);
         })
         return io;
     })
